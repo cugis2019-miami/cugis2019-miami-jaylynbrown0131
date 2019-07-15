13639e3de488829c8d0406384e366dc0f2dbea0a
jaylynbrown0131 <52932169+jaylynbrown0131@users.noreply.github.com>
2019-07-15 12:53:06 -0700

    Add files via upload

---
+++ Jaylyn GIS d1.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+sum =4+7
+
+print(sum)
+
+def summation(a,b):
+    sum = a+b
+    print(sum)
+    
+summation(9,6)    
+pwd
+3+11 #addition
+print("hello world")
+print("I am learning code in Python today")
+print(53)
+"hello world"
+5*2 #multiply
+5/2 #division
+5-2 #subtraction
+5**2 #exponent
+8/9*3 #division #multiplication
+3*9/8
+(8/9)*3
+print("the division of 5 by 2 is 2.5")
+print("the division of 5 by 3 is )
+print("the division of 5 by 3 is", 5/3)
+
+def plus (a,b):
+    plus= a+b
+    print(plus)
+    
+def plus(2+3):
+    plus= 2+3
+    print(plus)
+
+#create function to find the difference of two numbers 
+def Subtract(a,b):
+    Subtract= a-b
+    print(Subtract)
+    
+    Subtract(80,60)
+    
+def tri(a,b,c):
+    tri = a*b*c
+    print("the area of a triangle with base",b,"and height",c,"is", tri)
+    
+    tri(1/,2,2,4)
